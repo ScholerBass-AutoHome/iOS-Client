cbb65e0f07151c1be2b6a2926f40bf633d2b6774
Ben Scholer <benscholer3248511@gmail.com>
2015-03-15 23:58:35 -0400

    Added Appliance class

---
+++ AutoHome iOS Client/Appliance.swift
@@ -0,0 +1,15 @@
+//
+//  Appliance.swift
+//  AutoHome iOS Client
+//
+//  Created by Ben Scholer on 3/15/15.
+//  Copyright (c) 2015 AutoHome. All rights reserved.
+//
+
+import Foundation
+
+struct Appliance {
+    var name: String
+    var GPIOpin: Int
+    var status: Bool
+}
